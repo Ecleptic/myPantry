8cc83db0baece66578beed0c81e030eb07e0bfae
Cameron Green <cgree78@gmail.com>
2017-10-07 19:14:41 -0500

    The createTables script is coded a little bit better and checks for the tables already existing.

--- createTables.js
+++ createTables.js
@@ -4,48 +4,93 @@ const pgp = require('pg-promise')({promiseLib: promise})
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pantry'
 const client = pgp(connectionString)
 
-
 /**
      * Creates the tables
      */
 function createTables() {
-    let total = 0
+    createListsTable()
+    createFoodsTable()
+    createUsersTable()
+}
+
+function createListsTable() {
     client
-        .any(`CREATE TABLE "public"."users" (
-            "username" text,
-            "password" text,
-            "listName" text,
-            PRIMARY KEY ("username")
-        )`)
-        .then(() => {
-            console.log('Created Users Table')
-            total++
+        .any(`SELECT EXISTS (
+        SELECT 1
+        FROM   pg_tables
+        WHERE  pg_tables.tablename = 'lists'
+        );
+     `)
+        .then((e) => {
+            console.log("Lists Table exists: " + (e[0].exists))
+            if (!e[0].exists) {
+                client
+                    .any(`CREATE TABLE "public"."lists" (
+                    "username" text,
+                    "foodName" text,
+                    "qty/weight" text,
+                    PRIMARY KEY ("username")
+                )`)
+                    .then(() => {
+                        console.log('Created Lists Table')
+                    })
+            } else {
+                console.log("Lists Table Already Exists")
+            }
         })
+}
+function createUsersTable() {
     client
-        .any(`CREATE TABLE "public"."foods" (
-            "foodName" text,
-            "category" text,
-            "type" text,
-            "expiration" date,
-            PRIMARY KEY ("foodName")
-        )`)
-        .then(() => {
-            console.log('Created Foods Table')
-            total++
+        .any(`SELECT EXISTS (
+    SELECT 1
+    FROM   pg_tables
+    WHERE  pg_tables.tablename = 'users'
+    );
+ `)
+        .then((e) => {
+            console.log("Users Table exists: " + (e[0].exists))
+            if (!e[0].exists) {
+                client
+                    .any(`CREATE TABLE "public"."users" (
+                    "username" text,
+                    "password" text,
+                    "listName" text,
+                    PRIMARY KEY ("username")
+                )`)
+                    .then(() => {
+                        console.log('Created Users Table')
+                    })
+            } else {
+                console.log("Users Table Already Exists")
+            }
         })
+}
+function createFoodsTable() {
     client
-        .any(`CREATE TABLE "public"."lists" (
-            "username" text,
-            "foodName" text,
-            "qty/weight" text,
-            PRIMARY KEY ("username")
-        )`)
-        .then(() => {
-            console.log('Created lists Users Table')
-            total++
+        .any(`SELECT EXISTS (
+    SELECT 1
+    FROM   pg_tables
+    WHERE  pg_tables.tablename = 'foods'
+    );
+ `)
+        .then((e) => {
+            console.log("Foods Table exists: " + (e[0].exists))
+            if (!e[0].exists) {
+                client
+                    .any(`CREATE TABLE "public"."foods" (
+                    "foodName" text,
+                    "category" text,
+                    "type" text,
+                    "expiration" date,
+                    PRIMARY KEY ("foodName")
+                )`)
+                    .then(() => {
+                        console.log('Created Foods Table')
+                    })
+            } else {
+                console.log("Foods Table Already Exists")
+            }
         })
 }
 
-// connectDB()
 createTables()
-// disconnectDB()
