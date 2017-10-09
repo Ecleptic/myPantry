f6a58258e4baac700377445814d6a20c2e394d2a
Cameron Green <cgree78@gmail.com>
2017-10-09 14:09:46 -0500

    hopefully can help find the dblink file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 
 const port = (process.env.PORT || 8080)
-const dbLink = require('./dblink')
+const dbLink = require('./dblink.js')
 const db = new dbLink()
 
 // Setting up CORS headers
